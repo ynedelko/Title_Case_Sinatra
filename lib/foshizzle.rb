99cd294a51ddda6f35fc8be339f486b6c4aa0ac5
Josue Valverde & Andrew Haupt <ajhaupt7@gmail.com>
2015-08-03 13:57:07 -0700

    Finished foshizzle

--- lib/foshizzle.rb
+++ lib/foshizzle.rb
@@ -1,5 +1,23 @@
 class String
   define_method(:foshizzle) do
-    gsub!("s", "z")
-  end
+      words = split(" ").each() do |word|
+        idx = 1
+        while idx.<(word.length())
+          if word[idx].eql?("s")
+            word[idx] = "z"
+          end
+          idx = idx.+(1)
+        end
+      end
+      words.join(" ")
+    end
 end
+
+
+
+
+#words = split(" ").each() do |word|
+#  word.gsub!("s", "z")
+#end
+#words.join(" ")
+#end
