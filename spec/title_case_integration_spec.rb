33421e88a8743bfbb08c32aedfb9b0267fbd8a8c
Yelena Nedelko & Andrew Haupt <student@epicodus.com>
2015-08-05 10:22:54 -0700

    Add title.erb file to views folder

--- spec/title_case_integration_spec.rb
+++ spec/title_case_integration_spec.rb
@@ -1,6 +1,7 @@
 require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
 
 describe('the title case path', {:type => :feature}) do
   it("processes the user entry and returns it title cased") do
